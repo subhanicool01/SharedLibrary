dfe23f7bce685d0e4b811f31b10076b9e9564e0a
subhanicool01 <subhanicool01@gmail.com>
2024-11-27 11:56:59 +0530

    Update k8s.groovy

--- src/.com/builds/k8s.groovy
+++ src/.com/builds/k8s.groovy
@@ -4,11 +4,12 @@ class k8s {
        this.jenkins = jenkins
   }
 
-  def auth_login () {
+  def auth_login (gke_cluster_name, gke_zone, gke_project_name) {
     jenkins.sh """#!/bin/bash
     echo "entering into kubernetes gke cluster"
     gcloud config set account <service-account-name>
-    gcloud auth activate-service-account <service-account-name>
+    #gcloud auth activate-service-account <service-account-name>
+    gcloud container cluster get-credentials $gke_cluster_name --zone $gke_zone --project $gke_project_name
     """
   }
 }
